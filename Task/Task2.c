4a3bba33ab4ae48e3702b26f427327a998d3c72d
Maxim <maxim_sdrv@mail.ru>
2021-03-12 18:05:17 +0300

    added png file and code Task.c file

--- Task/Task2.c
+++ Task/Task2.c
@@ -2,8 +2,23 @@
 #include <math.h>
 
 
+int CheckDecimal(const int n){
+    if (n < 2) return 0;
+    for (int i = 2; i < sqrt(n); ++i){
+        if (n % i == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int main() {
-    
+
+    int n = 1;
+    if (CheckDecimal(n)){
+        printf("True\n");
+    }
+    else printf("False\n");
 
     return 0;
 }
