f8ac1c3f9c6f6d7a3d3a2e4f8c6ed92c3a6775cc
maxsdrv <34602478+maxsdrv@users.noreply.github.com>
2021-03-16 20:05:22 +0300

    Update Task2.c

--- Task/Task2.c
+++ Task/Task2.c
@@ -2,8 +2,7 @@
 #include <math.h>
 
 
-int CheckDecimal(const int n){
-    if (n < 2) return 0;
+int CheckPrime(const int n){
     for (int i = 2; i < sqrt(n); ++i){
         if (n % i == 0){
             return 0;
