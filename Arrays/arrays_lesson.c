5157f6df4932cfee1c7f5569ba10130b843ebe56
maxim <maxim_sdrv@mail.ru>
2021-03-19 22:29:17 +0300

    added new branch and folder with unfulfilled homework

---
+++ Arrays/arrays_lesson.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+//Инициализируем 2-мерный массив
+int** init2dIntArray(int** arr, const int row, const int col) {
+    arr = (int**) calloc(sizeof(int*), row);
+    for (int i = 0; i < row; ++i) {
+        *(arr + i) = (int*) calloc(sizeof(int), col);
+    }
+    return arr;
+}
+
+//Заполняем 2-мерный массив
+void fill2dIntArray(int** arr, const int row, const int col, const int value) {
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            *((*(arr + i)) + j) = rand() % value;
+        }
+    }
+}
+
+//Печать массива
+void print2dIntArray(int** arr, const int row, const int col, int offset) {
+    char format[7];
+    sprintf(format, "%%%dd", offset);
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            printf(format, *((*(arr + i)) + j));
+        }
+        printf("\n");
+    }
+}
+
+//Функция swap
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sort2dIntArray(int** arr, const int row, const int col) {
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            int m = i; // текущий столбец
+            int n = j + 1; //следующий элемент
+            while (true) {
+                if (n == col) { //здесь идёт проверка на границы массива
+                    n = 0;
+                    m++;
+                    if (m == row) break;// по достижению конца массива выходим из цикла while
+                }
+                if (arr[i][j] > arr[m][n]) {
+                    swap(&arr[i][j], &arr[m][n]);
+                }
+                n++;
+            }
+        }
+    }
+}
+
+//Функция для очистки выделенной памяти
+void freeAllocation(int** arr, const int row) {
+    for (int i = 0; i < row; ++i) {
+        free(arr[i]);
+    }
+    free(arr);
+}
+
+//Основная функция вызовов 
+void lesson_arrays() {
+    const int row = 4;
+    const int col = 5;
+    const int length = 10;
+    int** array = (int **) init2dIntArray(array, row, col);
+    fill2dIntArray(array, row, col, 100);
+    print2dIntArray(array, row, col, 3);
+    printf("\n");
+    sort2dIntArray(array, row, col);
+    print2dIntArray(array, row, col, 3);
+    freeAllocation(array, row);
+
+}
+
+int main() {
+
+   lesson_arrays();
+
+
+    return 0;
+}
