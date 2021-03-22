17b985675be8ad949829d82f94efac931e8ed17d
maxim <maxim_sdrv@mail.ru>
2021-03-22 14:40:12 +0300

    update

--- HardSorts/hard_sorts.c
+++ HardSorts/hard_sorts.c
@@ -28,13 +28,35 @@ void qs(int* array, const int first, const int last) {
     if (first < j) qs(array, first, j);
 }
 
+void sortInserts(int* arr, int len) {
+    int temp = 0, pos = 0;
+    for (int i = 1; i < len; ++i) {
+        temp = arr[i];
+        pos = i - 1;
+        while (pos >= 0 && arr[pos] > temp) {
+            arr[pos + 1] = arr[pos];
+            pos--;
+        }
+        arr[pos + 1] = temp;
+    }
+}
+
+void _quicksort(int *arr, int first, int last) {
+    int i = first; 
+    int j = last;
+    const int redirect = 10;
+    const int size_arr = ++last;
+    printf("size arr = %d\n", size_arr);
+}
 
 void hard_sorts() {
-    const int length = 10;
+    const int length = 20;
     int* array = init(array, length);
     fill(array, length);
     print(array, length, 3);
-    qs(array, 0, length - 1);
+    // qs(array, 0, length - 1);
+    // sortInserts(array, length);
+    _quicksort(array, 0, length - 1);
     print(array, length, 3);
     
 }
