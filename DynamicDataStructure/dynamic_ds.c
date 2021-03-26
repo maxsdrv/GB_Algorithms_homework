5db88733cf11d3ae18a83a0d548691531e43b543
maxim <maxim_sdrv@mail.ru>
2021-03-26 14:47:56 +0300

    update

--- DynamicDataStructure/dynamic_ds.c
+++ DynamicDataStructure/dynamic_ds.c
@@ -2,42 +2,61 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define T char
-#define Size  1000
+#define Size 1000
 
 
-int cursor = -1;
-T Stack[Size];
+typedef struct PriorityQueue {
+    int items;
+    int top;
+    int tail;
+    int queue[Size];
+} priority_queue;
 
-bool push(T data) {
-    if (cursor < Size) {
-        Stack[++cursor] = data;
-        return true;
-    }
-    else {
-        printf("Stacl overflow\n");
+
+bool Enqueue(priority_queue *pr, int data) {
+    if (pr->items == Size) {
+        printf("Queue is full\n");
         return false;
     }
+    if (pr->tail == Size - 1) {
+        pr->tail = -1;
+    } 
+    pr->queue[++pr->tail] = data;
+    pr->items++;
+    return true;
 }
 
-T pop() {
-    if (cursor != -1) {
-        return Stack[cursor--];
-    }
-    else {
-        printf("Stack is empty\n");
+int dequeue(priority_queue *pr) {
+    if (pr->items == 0) {
+        printf("Queue is empty\n");
         return -1;
     }
+    else {
+        int tmp = pr->queue[pr->top++];
+        pr->top %= Size;
+        pr->items--;
+        return tmp;
+    }
 }
 
-int main() {
-   push('a');
-   push('b');
-   push('c');
-   while (cursor != -1) {
-       printf("%c\n", pop());
-   }
+void InitStruct(priority_queue *pr) {
+    pr->items = 0;
+    pr->tail = -1;
+    pr->top = 0;
+}
 
+
+int main() {
+    priority_queue pr;
+    InitStruct(&pr);
+    Enqueue(&pr, 1);
+    Enqueue(&pr, 2);
+    Enqueue(&pr, 3);
+    Enqueue(&pr, 4);
+    Enqueue(&pr, 5);
+    while (pr.items > 0) {
+        printf("%d ", dequeue(&pr));
+    }
 
     return 0;
 }
