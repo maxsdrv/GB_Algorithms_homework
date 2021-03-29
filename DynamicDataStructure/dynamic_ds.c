a565c0cc8cd2d68450601f2fadfcb927d9e16186
maxim <maxim_sdrv@mail.ru>
2021-03-29 14:10:07 +0300

    added homework for Lesson9

--- DynamicDataStructure/dynamic_ds.c
+++ DynamicDataStructure/dynamic_ds.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 
 #define Size 10
+#define sizeStack 1000
+#define T int
 
 typedef struct {
     int priority;
@@ -71,36 +73,26 @@ void InsertTo(Auxiliary *a, const int pr, const int data) {
     
 }
 
-int GetIndex(Node *item) {
-    for (int i = 0; i < Size; ++i) {
-        if (array[i] == item) {
-            return i;
-        }
-    }
-    return -1;
-}
 
 Node* Remove(Auxiliary *a) {
     int index = 0;
-    Node *maxPrior = array[0]; 
+    int maxPos = array[0]->priority;
     if (a->items == 0) {
         return NULL;
     }
-    for (int i = 1; i <= a->tail; ++i) {
-        if (array[i] == NULL) break;
-        if (maxPrior->priority > array[i]->priority) {
-            maxPrior = array[i];
+    for (int i = 1; i < a->tail; ++i) {
+        if (maxPos > array[i]->priority) {
+            maxPos = array[i]->priority;
+            index = i;
         }
     }
-    index = GetIndex(maxPrior);
-    if (index == -1) printf ("Something went wrong.../n");
-    Node *temp = array[index]; 
+    Node *temp = array[index];
     array[index] = NULL;
+
     a->items--;
     a->tail--;
 
     return temp;
-    
 }
 
 void PrintQueue(Auxiliary *a) {
@@ -116,10 +108,53 @@ void PrintQueue(Auxiliary *a) {
     printf("\n");
 }
 
+/* __________________________________________________________*/
+int top = -1;
+T Stack[Size];
+
+bool PushStack(int data) {
+    if (top < Size) {
+        Stack[++top] = data;
+        return true;
+    }
+    else {
+        printf("Stack overflow\n");
+        return false;
+    }
+}
+
+
+T PopStack() {
+    if (top != -1) {
+        return Stack[top--];
+    }
+    else {
+        printf("Stack is empty\n");
+        return -1;
+    }
+}
+
+void DecToBin(int number) {
+    while (number > 0) {
+        int remainder = number % 2;
+        number /= 2;
+        PushStack(remainder);
+    }
+    while (top != -1) {
+        int item = 0;
+        item = PopStack();
+        printf("%d", item);
+    }
+    printf("\n");
+}
+
+
+
 int main() {
 /*Task1 Описать очередь с приоритетным исключением*/
 //Testing
-{    
+{   
+    printf("Start test Task1 ....\n");
     Auxiliary a;
     InitStruct(&a);
     InsertTo(&a, 3, 1);
@@ -136,10 +171,26 @@ int main() {
     PrintQueue(&a);
     InsertTo(&a, 5, 5);
     PrintQueue(&a);
+    printf("End test Task1.\n");
+    /*Не знаю  почему коряво освобождает память, по отладчику 
+    в массиве мусор, а в консоль выводит значения почему-то*/
+    for (int i = 0; i < a.tail; ++i) {
+        printf("%d %d\n", array[i]->priority, array[i]->data);
+        free(array[i]);
+    }
+    for (int i = 0; i < a.tail; ++i) {
+        printf("%d %d\n", array[i]->priority, array[i]->data);
+    }
 }
 
-
-
+/* Task2. Реализовать перевод из десятичной в двоичную
+ систему счисления с использованием стека. */
+    int number = 10;
+    printf("Convert a Decimal number to Binary number\n");
+    printf("Decimal Number = %d \n", number);
+    printf("Binary number = ");
+    DecToBin(number);
+    printf("\n");
     
     return 0;
 }
