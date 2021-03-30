f1a931e387d3108ef8202d5522a857f651bebc8c
maxim <maxim_sdrv@mail.ru>
2021-03-30 14:20:03 +0300

    added task10.1

--- Task7/homework.c
+++ Task7/homework.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "mystack.h"
 
-int main() {
+
+int checkLeftBrackets(char brackets) {
+    return (brackets == '(' || brackets == '[' || brackets == '<');
+}
+
+int checkRightBrackets(char brackets) {
+    return (brackets == ')' || brackets == ']' || brackets == '>');
+}
+
+bool CorrectBrackets(char* brackets) {
     Stack *st = (Stack*) malloc(sizeof(Stack));
     init(st);
-    PushStack(st, 'a');
-    PushStack(st, 'b');
-    PushStack(st, 'c');
-    PushStack(st, 'd');
-    PushStack(st, 'f');
-    printOneLinkCharStack(st);
+    char temp;
+    PushStack(st, brackets[0]);
+    for (int i = 1; i < strlen(brackets) + 1; ++i) {
+        if (checkLeftBrackets(brackets[i])) {
+            PushStack(st, brackets[i]);
+        }
+        else if (checkRightBrackets(brackets[i])) {
+            PopStack(st);
+        }
+        else return false;
+    }
+    if (isEmpty(st)) {
+        return true;
+    }
+    else return false;
+}
 
+int main() {
+//    printOneLinkCharStack(st);
+    char array[] = "(2/{5*(4+7)]";
+    printf("Result = %d\n", CorrectBrackets(array));
 
     return 0;
 }
